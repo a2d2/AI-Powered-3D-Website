aa6907f9e4e71ce89263d46125240afb5296ebd8
r2d2 <arturo.galvis@hotmail.com>
2023-06-02 14:08:11 -0500

    6-color-picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  const snap = useSnapshot(state);
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        // presetColors={['#00000', '#fffff', '#00ff00', '#0000ff']}
+        onChange={(color) => (state.color = color.hex)}
+      />
+    </div>
+  );
 };
 export default ColorPicker;
